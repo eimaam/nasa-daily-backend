6e34678e46d1fa5ebaf1f3265af308655d8a0906
eimaam <imamddahir@gmail.com>
2023-01-19 14:28:22 +0100

    feat: created server data

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const app = express()
+const connectDb = require("./Database/db")
+const userRoute = require("./Route/router")
+const dotenv =  require("dotenv").config
+const cors = require("cors")
+
+
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static("./"))
+
+app.use("/api/v1/user", userRoute)
+app.use("/", (req, res)=>{
+    res.json({})
+})
+
+const port = process.env.PORT || 5000
+
+const start = async ()=>{
+
+   try {
+    await connectDb("mongodb+srv://eimaam:eimaam@cluster0.8xmtwqu.mongodb.net/Nasa-daily?retryWrites=true&w=majority")
+    app.listen(port, ()=>{
+        console.log(`server is running at port ${port}`)
+    })
+   } catch (error) {
+    console.log(error)
+   }
+}
+start()
